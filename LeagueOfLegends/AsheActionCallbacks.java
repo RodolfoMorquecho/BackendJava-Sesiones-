cea9f044cb2e10c162a5118913abe78811d11d09
RodolfoMorquecho <99112892+RodolfoMorquecho@users.noreply.github.com>
2022-05-13 00:55:03 -0500

    Delete AsheActionCallbacks.java

--- LeagueOfLegends/AsheActionCallbacks.java
+++
@@ -1,8 +0,0 @@
-public interface AsheActionCallbacks {
-    //Lo que hace Ashe(LOL)
-    void frostShot(Screen s);  //Le pasamos un objeto 's' de la clase "Screen" porque queremos trabajar con la interface grafica
-    void hawkShot(Screen s);
-    void rangerFocus(Screen s);
-    void volley(Screen s);
-    void crystalArrow(Screen s);
-}
